8ce5146a17c382cd1ee30bf82ebe7e083343e182
yywc <gyjyywc@gmail.com>
2019-06-23 00:04:46 +0800

    fix(class birds): 小鸟可以飞出上屏幕&游戏难度降低

--- src/modules/runtime/PencilDown.ts
+++ src/modules/runtime/PencilDown.ts
@@ -8,7 +8,7 @@ export default class PencilDown extends Pencil {
 
   public draw(): void {
     // gap 值，屏幕高度的一部分，根据自己喜好调整，值越大游戏越容易
-    const gapBetweenTwoPencils = window.innerHeight / 8;
+    const gapBetweenTwoPencils = window.innerHeight / 7;
     // 最终下铅笔（我们可以看到）的高度，top 的值加上 gap 值
     this.dy = this.top + gapBetweenTwoPencils;
     super.draw();
